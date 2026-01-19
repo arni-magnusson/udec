ca38281cadd5084090f9f638eec72761b27524ea
Arni Magnusson <thisisarni@gmail.com>
2026-01-19 09:08:22 -0300

    Add computing

---
+++ slides/1b_computing/script/computing.R
@@ -0,0 +1,17 @@
+# Calculator with functions
+2 + 2
+sqrt(10)
+log(10)
+
+# Objects in workspace
+x <- 2
+10 * x
+ls()
+rm(x)
+rm(list=ls())
+
+# Plots
+x <- 1:10
+y <- 3 * x
+plot(x, y)
+y <- 100 * x
