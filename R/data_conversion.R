90d7247e8e30ed2fa8dcee2e7694c7a87a3ae787
HansTtito <kvttitos@gmail.com>
2026-01-20 17:26:55 -0300

    Add data conversion analysis

--- R/data_conversion.R
+++ R/data_conversion.R
@@ -6,7 +6,7 @@ milton <- read.xlsx("../data-raw/Fishery_base_Milton_Montufar.xlsx", sheet=1)
 
 library(readxl)
 also1 <- read_excel("../data-raw/BKC_project_MB&SF.xlsx", sheet=1)
-aswell1 <- read_excel("../data-raw/LIXIVIACIÓN.xlsx", sheet=1,
+aswell1 <- read_excel("data-raw/LIXIVIACIÓN.xlsx", sheet=1,
                       range="C80:L95")
 
 
