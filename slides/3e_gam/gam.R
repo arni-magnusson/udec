99c7deb4d87d34b28fa52040af7e0b5680e0e08a
Arni Magnusson <thisisarni@gmail.com>
2026-01-21 14:10:08 -0300

    Add 3e_gam

---
+++ slides/3e_gam/gam.R
@@ -0,0 +1,16 @@
+## Fit LM to data
+plot(mpg~hp, data=mtcars)
+model.lm <- lm(mpg~hp, data=mtcars)
+abline(model.lm)
+summary(model.lm)
+
+## Fit GAM to data
+require(mgcv)
+model.gam <- gam(mpg~s(hp), data=mtcars)
+xvec <- seq(min(mtcars$hp), max(mtcars$hp))
+y.gam <- predict(model.gam, data.frame(hp=xvec))
+lines(xvec, y.gam, lwd=2, col="red")
+summary(model.gam)
+
+## Compare LM and GAM using AIC
+AIC(model.lm, model.gam)
