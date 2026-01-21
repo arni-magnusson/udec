3e7c892e79c0ba2c82fee72bae30de8e9067b53e
Arni Magnusson <thisisarni@gmail.com>
2026-01-21 13:51:21 -0300

    Rename aswell1 -> lixi2

--- R/data_conversion.R
+++ R/data_conversion.R
@@ -1,32 +1,40 @@
 library(openxlsx)
-sheet1 <- read.xlsx("../data-raw/BKC_project_MB&SF.xlsx", sheet=1)
-lixi1 <- read.xlsx("../data-raw/LIXIVIACIÓN.xlsx", sheet=1,
-                   rows=80:95, cols=3:12)
-milton <- read.xlsx("../data-raw/Fishery_base_Milton_Montufar.xlsx", sheet=1)
-
 library(readxl)
-also1 <- read_excel("../data-raw/BKC_project_MB&SF.xlsx", sheet=1)
-aswell1 <- read_excel("data-raw/LIXIVIACIÓN.xlsx", sheet=1,
-                      range="C80:L95")
-
-
 library(tidyr)
 library(dplyr)
 
-aswell1 <- aswell1 |> gather(Treatment, Measurement, -c(Tiempo)) 
+# Import using openxlsx
+sheet1 <- read.xlsx("../data-raw/BKC_project_MB&SF.xlsx", sheet=1)
+lixi1 <- read.xlsx("../data-raw/LIXIVIACIÓN.xlsx", sheet=1, rows=80:95,
+                   cols=3:12)
+milton <- read.xlsx("../data-raw/Fishery_base_Milton_Montufar.xlsx", sheet=1)
+libro2 <- read.xlsx("../data-raw/Libro 2.xlsx")
 
+# Import using readxl
+also1 <- read_excel("../data-raw/BKC_project_MB&SF.xlsx", sheet=1)
+lixi2 <- read_excel("../data-raw/LIXIVIACIÓN.xlsx", sheet=1, range="C80:L95")
 
-aswell1 |> 
+################################################################################
+
+# Reshape lixiviación dataset
+lixi2 <- lixi2 |> gather(Treatment, Measurement, -c(Tiempo))
+
+lixi2 <-
+lixi2 |>
   mutate(
-    Replicate = rep(rep(c(1,2,3), each = nrow(aswell1)/9), 3),
+    Replicate = rep(rep(c(1,2,3), each = nrow(lixi2)/9), 3),
     Treatment = case_when(
       Treatment %in% c("...3","...4") ~ "Control",
       Treatment %in% c("...6","...7") ~ "IER",
       Treatment %in% c("...9","...10") ~ "IEQ",
       .default = Treatment
     )
-  ) |> 
+  ) |>
   as.data.frame()
+
+# Compare lixi2 and libro2
+
+################################################################################
 
 # Verify integrity of CSV files
 HembrasTot <- read.csv("../data/HembrasTot.csv")
